e6165ecdb307881fe684626806cdf44bab4a47c4
Phong Yang <phongyang88@hotmail.com>
2020-05-26 12:52:21 -0500

    initial commit. created react app and changed app name

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import reactDOM from 'react-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      content  here
+    </div>
+  );
+}
+
+export default App;
