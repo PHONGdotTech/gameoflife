0ee91b685fab08ca5753c372e6003dbfee2be122
Phong Yang <phongyang88@hotmail.com>
2020-05-26 13:40:24 -0500

    converted main and grid components to functional components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Grid from "./Grid"
 
-class Main extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            generation: 0,
-        }
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>Conway's Game of Life</h1>
-                <Grid />
-                <h2>Generations: {this.state.generation}</h2>
-            </div>
-        )
-    }
+const Main = () =>{
+    const [generation, setGeneration] = useState(0)
+    
+    return (
+        <div>
+            <h1>Conway's Game of Life</h1>
+            <Grid />
+            <h2>Generations: {generation}</h2>
+        </div>
+    )
 }
 
 export default Main
