79dfd02fdc53f8d7215fd610c4631dbefce8122d
Phong Yang <phongyang88@hotmail.com>
2020-05-28 16:33:09 -0500

    Updates

---
+++ precache-manifest.441daa92c49f2f1b9606fd9bb060709c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa63cfe2199ebdff51a0de5d8949172",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "965701cbafde3ebcc96e",
+    "url": "/gameoflife/static/css/main.493dda9e.chunk.css"
+  },
+  {
+    "revision": "369d484c62ed10901ff2",
+    "url": "/gameoflife/static/js/2.ea9eb432.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gameoflife/static/js/2.ea9eb432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965701cbafde3ebcc96e",
+    "url": "/gameoflife/static/js/main.6c078485.chunk.js"
+  },
+  {
+    "revision": "c45ef81fdf33c718aed5",
+    "url": "/gameoflife/static/js/runtime-main.c9296233.js"
+  }
+]);
